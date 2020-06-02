34f8814ba81727fe4489ba1caba58355ed2172b1
Matheus Mazeto <mgmazeto@gmail.com>
2020-06-02 20:43:11 -0300

    feat(routes): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return <div>hello</div>;
-};
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Router>
+      <Routes />
+    </Router>
+  </>
+);
 
 export default App;
